7dc631b4988f12ba567a8f5157d1d979b2b362c9
Michael Lehning <mll8657@g.rit.edu>
2019-02-19 16:22:23 -0500

    Moved threading test code into its own file. Made template file for the main system (GarageDoorController)

--- garage_door_controller/GarageDoorController.cpp
+++ garage_door_controller/GarageDoorController.cpp
@@ -1,30 +1,21 @@
-#include <cstdlib>
-#include <iostream>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#define N 5
+/*
+ * GarageDoorController.cpp
+ *
+ *  Created on: Feb 19, 2019
+ *      Author: mll8657
+ */
 
-void *worker_thread(void *arg) {
-	printf("This is worker_thread #%ld\n", (long)arg);
-	pthread_exit(NULL);
+#include "GarageDoorController.h"
+
+void GarageDoorController::processInput(int input) {
+
 }
 
-int main(int argc, char *argv[]) {
-	printf("Hellow World!");
-	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
-	pthread_t my_thread[N];
+GarageDoorController::GarageDoorController() {
+	// TODO Auto-generated constructor stub
 
-	long id;
-	for(id = 1; id <= N; id++) {
-	        int ret =  pthread_create(&my_thread[id], NULL, &worker_thread, (void*)id);
-	        if(ret != 0) {
-	                printf("Error: pthread_create() failed\n");
-	                exit(EXIT_FAILURE);
-	        }
-	}
+}
 
-	pthread_exit(NULL);
-
-	return EXIT_SUCCESS;
+GarageDoorController::~GarageDoorController() {
+	// TODO Auto-generated destructor stub
 }
---
+++ garage_door_controller/GarageDoorController.h
@@ -0,0 +1,18 @@
+/*
+ * GarageDoorController.h
+ *
+ *  Created on: Feb 19, 2019
+ *      Author: mll8657
+ */
+
+#ifndef GARAGEDOORCONTROLLER_H_
+#define GARAGEDOORCONTROLLER_H_
+
+class GarageDoorController {
+public:
+	void processInput();
+	GarageDoorController();
+	virtual ~GarageDoorController();
+};
+
+#endif /* GARAGEDOORCONTROLLER_H_ */
